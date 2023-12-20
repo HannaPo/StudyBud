030d62725f6f2e1b0f1c7e1e01a7c0a3eb29a7b0
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-12-20 11:39:38 +0200

    init commit

---
+++ base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'}
+]
+
+def home(request):
+    context =  {'rooms': rooms}
+    return render(request, 'base/home.html',)
+
+def room (request):
+    return render(request, 'base/room.html')
